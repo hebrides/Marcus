cf06354c6cd84d2c91ce4654bb5607ef0f581881
Anaximander <marcus@smgmobile.com>
2024-03-20 12:53:37 -0400

    update privacy policy

--- web/privacy.php
+++ web/privacy.php
@@ -1,27 +1,45 @@
-Preamble:
+<h1>Data Protection Policy</h1>
 
-In our digital age, the tracking of individuals through technology has become prevalent, utilized by advertisers, governments, and security services alike. At the Aurelius Fund, we view such practices as fundamentally dystopian.
+<h2>Preamble</h2>
 
-We assert that security services must be required to obtain hard-to-acquire warrants prior to any surveillance activities. Every citizen has the right to freedom from the perils associated with unchecked governmental intrusion into their private lives.
+<p>
+  In our digital age, the tracking of individuals through technology has become prevalent, utilized by advertisers, governments, and security services alike. At the Aurelius Fund, we view such practices as fundamentally dystopian.
+</p>
 
-Moreover, the existence of private data brokerage stands as a stark affront to privacy, turning the joy of technology use into a source of unease. Surveillance-based advertising, in particular, is invasive and detracts from the integrity of digital experiences. There are far more honorable methods to generate revenue. Consumers rightfully deserve their privacy in every interaction with commercial products.
+<p>
+  We assert that security services must be required to obtain hard-to-acquire warrants prior to any surveillance activities. Every citizen has the right to freedom from the perils associated with unchecked governmental intrusion into their private lives.
+</p>
 
-We advocate for robust legislation that upholds privacy as an inalienable human right, as recognized in Article 12 of the Universal Declaration of Human Rights, ensuring its protection across both commercial and governmental spheres.
+<p>
+  Moreover, the existence of private data brokerage stands as a stark affront to privacy, turning the joy of technology use into a source of unease. Surveillance-based advertising, in particular, is invasive and detracts from the integrity of digital experiences. There are far more honorable methods to generate revenue. Consumers rightfully deserve their privacy in every interaction with commercial products.
+</p>
 
-Privacy Policy:
+<p>
+  We advocate for robust legislation that upholds privacy as an inalienable human right, as recognized in Article 12 of the Universal Declaration of Human Rights, ensuring its protection across both commercial and governmental spheres.
+</p>
 
-User Data and Privacy
+<h2>Data Protection & Privacy</h2>
+
+<h3>User Data and Privacy</h3>
+
+</p>
 The Aurelius Fund is committed to protecting the privacy of our users. We do not collect, store, or share any personal data through our website. Our commitment to your privacy is paramount, and we believe in the fundamental right to use our services without the compromise of user data collection.
+</p>
 
-Third-Party Services and Embedded Content
-Our site may use third-party services such as Google Fonts and PayPal to enhance user experience. While we strive to integrate only those services that uphold the highest privacy standards, we cannot guarantee the privacy practices of these third-party entities. Therefore, we recommend that users access our site using a VPN-enabled device to ensure an added layer of privacy and security.
+<h3>Third-Party Services and Embedded Content</h3>
 
-By using our website, you acknowledge the potential risks associated with embedded services and the limitations of our control over these third-party providers. We encourage users to remain informed and take proactive measures to protect their privacy while online.
+<p>
+  Our site may use third-party services such as Google Fonts and PayPal to enhance user experience. While we strive to integrate only those services that uphold the highest privacy standards, we cannot guarantee the privacy practices of these third-party entities. Therefore, we recommend that users access our site using a VPN-enabled device to ensure an added layer of privacy and security.
+</p>
 
-More information:
+<p>
+  By using our website, you acknowledge the potential risks associated with embedded services and the limitations of our control over these third-party providers. We encourage users to remain informed and take proactive measures to protect their privacy while online.
+</p>
 
-https://www.un.org/en/about-us/universal-declaration-of-human-rights
+<h2>More information</h2>
 
-https://youtu.be/vitbxi_36Ew
-
-https://en.wikipedia.org/wiki/Right_to_privacy
+<ul>
+  <li>https://www.un.org/en/about-us/universal-declaration-of-human-rights</li>
+  <li>https://youtu.be/vitbxi_36Ew</li>
+  <li>https://en.wikipedia.org/wiki/Right_to_privacy</li>
+</ul>
