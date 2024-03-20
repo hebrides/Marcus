f1c5efb9cdcde7b697d4df6ec3129aca9a2fa5bc
Anaximander <marcus@smgmobile.com>
2024-03-20 12:45:57 -0400

    update privacy policy

--- web/privacy.php
+++ web/privacy.php
@@ -1,16 +1,27 @@
+Preamble:
 
-We live in a world where tracking individuals via technology has become a tool not only for advertisers, but also for governments and security services.
+In our digital age, the tracking of individuals through technology has become prevalent, utilized by advertisers, governments, and security services alike. At the Aurelius Fund, we view such practices as fundamentally dystopian.
 
-We at the Aurelius Fund believe this activity is dystopian, no matter who does it.
+We assert that security services must be required to obtain hard-to-acquire warrants prior to any surveillance activities. Every citizen has the right to freedom from the perils associated with unchecked governmental intrusion into their private lives.
 
-Security services should obtain warrants that are hard to get before eavesdropping. Citizens deserve to be free from the evils that follow any state agent's unbridled access to our private lives.
+Moreover, the existence of private data brokerage stands as a stark affront to privacy, turning the joy of technology use into a source of unease. Surveillance-based advertising, in particular, is invasive and detracts from the integrity of digital experiences. There are far more honorable methods to generate revenue. Consumers rightfully deserve their privacy in every interaction with commercial products.
 
-Private data brokerage, honestly, should not exist. Surveillance advertising is creepy and makes using technology joyless. There's more honorable ways to make money. Consumers deserve privacy when in using any commercial product.
+We advocate for robust legislation that upholds privacy as an inalienable human right, as recognized in Article 12 of the Universal Declaration of Human Rights, ensuring its protection across both commercial and governmental spheres.
 
-Please support legislation to enforce privacy, our God-given human right, acknowledged in Article 12 of the Universal Declaration of Human Rights, in commercial and governmental activities.
+Privacy Policy:
+
+User Data and Privacy
+The Aurelius Fund is committed to protecting the privacy of our users. We do not collect, store, or share any personal data through our website. Our commitment to your privacy is paramount, and we believe in the fundamental right to use our services without the compromise of user data collection.
+
+Third-Party Services and Embedded Content
+Our site may use third-party services such as Google Fonts and PayPal to enhance user experience. While we strive to integrate only those services that uphold the highest privacy standards, we cannot guarantee the privacy practices of these third-party entities. Therefore, we recommend that users access our site using a VPN-enabled device to ensure an added layer of privacy and security.
+
+By using our website, you acknowledge the potential risks associated with embedded services and the limitations of our control over these third-party providers. We encourage users to remain informed and take proactive measures to protect their privacy while online.
+
+More information:
 
 https://www.un.org/en/about-us/universal-declaration-of-human-rights
 
-https://www.wired.com/story/how-pentagon-learned-targeted-ads-to-find-targets-and-vladimir-putin/
+https://youtu.be/vitbxi_36Ew
 
 https://en.wikipedia.org/wiki/Right_to_privacy
