cab5186dd6d0b2973058d3cd2a8ee87e9adcc783
Anaximander <marcus@smgmobile.com>
2024-05-21 10:44:13 -0400

    modal vis adjust

--- web/index-reader.php
+++ web/index-reader.php
@@ -232,12 +232,13 @@ main {
 .modal-content {
     position: relative;
     box-shadow: 0 4px 6px rgba(0,0,0,0.1);
-    width: 90vw;
+    width: 92vw;
     height: 90vh;
     display: flex;
     flex-direction: column;
-    background: #123456;
-    border: 1px solid rgba(255,255,255,.5);
+    background-color: #112233;
+    border: 1px solid rgba(255,255,255,.1);
+    box-shadow: 0 8px 16px rgba(0,0,0,0.2);
     overflow-y: hidden;
     z-index: 51;
 }
@@ -247,10 +248,15 @@ main {
     top: 0;
     background: inherit;
     padding: 10px 10px 5px 15px;
-    border-bottom: 1px solid rgba(255,255,255,.5);;
+    border-bottom: 1px solid rgba(255,255,255,.1);;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    color: #e9e9e9;
+}
+
+.modal-header > h2 {
+    font-weight: normal;
 }
 
 .modal-body {
